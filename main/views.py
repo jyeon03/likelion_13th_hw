e4b58770174694de4840c5caf645e5524cda0035
jyeon03 <bjlyeon2003@naver.com>
2025-04-08 00:11:44 +0900

    Submit: likelion_13th_task_week2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request, 'main/mainpage.html') 
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html') 
+
+# Django가 templates 폴더에서 mainpage.html을 찾도록 설정되어 있음.
